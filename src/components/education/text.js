a18fec60979ec27c67f1ad42ac602e57e7694630
Joel Detweiler <joel.detweiler@gmail.com>
2025-05-21 21:21:51 -0700

    Update resume 2025

--- src/components/education/text.js
+++ src/components/education/text.js
@@ -1,24 +1,22 @@
 const schools = [
   {
-    name: 'University of Illinois',
-    location: 'Urbana, IL',
-    degree: 'Master of Computer Science - Data Science track',
-    date: 'May 2020',
-    items: [
-      'GPA: 3.90'
-    ],
+    name: "University of Illinois",
+    location: "Urbana, IL",
+    degree: "Master of Computer Science - Data Science",
+    date: "May 2020",
+    items: ["GPA: 3.90"],
   },
   {
-    name: 'Pomona College',
-    location: 'Claremont, CA',
-    degree: 'Bachelor of Arts, Computer Science',
-    date: 'May 2012',
+    name: "Pomona College",
+    location: "Claremont, CA",
+    degree: "Bachelor of Arts, Computer Science",
+    date: "May 2012",
     items: [
-      'Minor: Mathematics',
-      'Thesis: Updatable Learning of Support Vector Machines',
+      "Minor: Mathematics",
+      "Thesis: Updatable Learning of Support Vector Machines",
       // '3 Years as a Computer Science T.A.'
     ],
   },
-]
+];
 
-export default schools
+export default schools;
--- src/components/skills/text.js
+++ src/components/skills/text.js
@@ -1,53 +1,34 @@
 export const skills = [
   {
-    name: 'JS/Typescript',
+    name: "JS/Typescript",
     value: 99,
   },
   {
-    name: 'MongoDB',
-    value: 99,
+    name: "PostgreSQL",
+    value: 97,
   },
   {
-    name: 'REST APIs',
+    name: "MongoDB",
+    value: 97,
+  },
+  {
+    name: "REST APIs",
     value: 95,
   },
   {
-    name: 'Architecting',
+    name: "Architecting",
     value: 95,
   },
   {
-    name: 'PostgreSQL',
-    value: 90,
-  },
-  {
-    name: 'React',
+    name: "Mentorship",
     value: 85,
   },
+  { name: "DevOps", value: 75 },
   {
-    name: 'Management',
-    value: 85,
-  },
-  {
-    name: 'AWS',
-    value: 80,
-  },
-  {
-    name: 'Java',
+    name: "React",
     value: 75,
   },
-  {
-    name: 'Scala',
-    value: 75,
-  },
-  {
-    name: 'Sass',
-    value: 70,
-  },
-  {
-    name: 'Python',
-    value: 70,
-  },
-]
+];
 
 export const intro =
-  'I am a well-rounded full-stack developer, comfortable with front and back end development, databases, css, and basic DevOps. I am experienced as the lead developer on new projects, where I have built high-performance products as well as internal tools and packages. I am flexible and enjoy staying up to date on new technologies and frameworks. My goal is always to keep projects well-tested, performant, and maintainable.'
+  "I am a well-rounded full-stack developer, comfortable with front and back end development, databases, css, and basic DevOps. I am experienced as the lead developer on new projects, where I have built high-performance products as well as internal tools and packages. I am flexible and enjoy staying up to date on new technologies and frameworks. My goal is always to keep projects well-tested, performant, and maintainable.";
